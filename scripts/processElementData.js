a1b48a394f27f48ea59d9b82a9bc05613d5a244b
joaovitorx6 <joaovitorx6@gmail.com>
2021-04-21 21:54:50 -0300

    Subindo projeto no EPF

---
+++ scripts/processElementData.js
@@ -0,0 +1,102 @@
+//------------------------------------------------------------------------------
+// Copyright (c) 2005, 2006 IBM Corporation and others.
+// All rights reserved. This program and the accompanying materials
+// are made available under the terms of the Eclipse Public License v1.0
+// which accompanies this distribution, and is available at
+// http://www.eclipse.org/legal/epl-v10.html
+// 
+// Contributors:
+// IBM Corporation - initial implementation
+//------------------------------------------------------------------------------
+
+// This file will include dynamically generated process layout related data from 
+// the publishing service 
+contentPage.processPage.imageFiles["_gnmccaIwEeuEsYxgxLaCHgActivityDetail"]="./../../designthinking/capabilitypatterns/resources/Imersao_9B805462_9b805462_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHgActivity"]="./../../designthinking/deliveryprocesses/resources/DesignThinking_FA13E873_fa13e873_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_oUDN8KI0EeuEsYxgxLaCHg,_tav2caI-EeuEsYxgxLaCHgActivity"]="./../../designthinking/deliveryprocesses/resources/Apresentacao_9A2D7384_e7c20002_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_oUDN8KI0EeuEsYxgxLaCHg,_tav2caI-EeuEsYxgxLaCHgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Apresentacao_9A2D7384_e7c20002_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_osLPEKKtEeubIrsk_5dxGg,_r1ynMaK0EeubIrsk_5dxGgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Implantar o produto_EC4762A4_28acdead_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_xhTKwaKvEeubIrsk_5dxGgActivity"]="./../../designthinking/deliveryprocesses/resources/Testar_1854ED3_2a585c9a_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_oUDN8KI0EeuEsYxgxLaCHg,_bSyaQaI4EeuEsYxgxLaCHgActivity"]="./../../designthinking/deliveryprocesses/resources/Selecionar a ideia_90545E6D_dde8eaeb_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_oUDN8KI0EeuEsYxgxLaCHg,_bSyaQaI4EeuEsYxgxLaCHgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Selecionar a ideia_90545E6D_dde8eaeb_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_pabnMaI0EeuEsYxgxLaCHgActivity"]="./../../designthinking/deliveryprocesses/resources/Imersao_C46982F1_ed3c90b8_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_mnRjEaI0EeuEsYxgxLaCHgActivity"]="./../../designthinking/deliveryprocesses/resources/Desenvolvimento_FF46CCB5_2819da7c_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_qSuhIaI0EeuEsYxgxLaCHg,_pcvFkaI6EeuEsYxgxLaCHgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Prototipar a solucao_F7DDE45F_ae81ab07_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_pabnMaI0EeuEsYxgxLaCHg,_BMlGYaI4EeuEsYxgxLaCHgActivity"]="./../../designthinking/deliveryprocesses/resources/Identificar requisitos_F2108A12_6999011e_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_pabnMaI0EeuEsYxgxLaCHg,_BMlGYaI4EeuEsYxgxLaCHgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Identificar requisitos_F2108A12_6999011e_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_osLPEKKtEeubIrsk_5dxGg,_wA9nIaK0EeubIrsk_5dxGgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Divulgacao do produto_5BB63F93_981bbb9c_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_pabnMaI0EeuEsYxgxLaCHg,_FkHj0aI3EeuEsYxgxLaCHgActivity"]="./../../designthinking/deliveryprocesses/resources/Empatizar_3C64D07B_b3ed4787_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_pabnMaI0EeuEsYxgxLaCHg,_FkHj0aI3EeuEsYxgxLaCHgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Empatizar_3C64D07B_b3ed4787_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_pabnMaI0EeuEsYxgxLaCHg,_zqSmEaI3EeuEsYxgxLaCHgActivity"]="./../../designthinking/deliveryprocesses/resources/Compreensão_580F61B0_cf97d8bc_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_pabnMaI0EeuEsYxgxLaCHg,_zqSmEaI3EeuEsYxgxLaCHgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Compreensão_580F61B0_cf97d8bc_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_xhTKwaKvEeubIrsk_5dxGg,_Zm3_saK0EeubIrsk_5dxGgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Testar a solucao_E02A649_e159e337_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_qSuhIaI0EeuEsYxgxLaCHgActivity"]="./../../designthinking/deliveryprocesses/resources/Prototipacao_E74EE68D_1021f454_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_mnRjEaI0EeuEsYxgxLaCHg,_8boqcaI4EeuEsYxgxLaCHgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Construir solucao_DBEFC56E_2457a3e_ActivityDetail.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_osLPEKKtEeubIrsk_5dxGgActivity"]="./../../designthinking/deliveryprocesses/resources/Implantacao_91DB6EEE_baae7cb5_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_oUDN8KI0EeuEsYxgxLaCHgActivity"]="./../../designthinking/deliveryprocesses/resources/Ideacao_BDE44E63_e6b75c2a_Activity.jpeg"
+contentPage.processPage.imageFiles["_fO-f0aI0EeuEsYxgxLaCHg,_oUDN8KI0EeuEsYxgxLaCHgActivityDetail"]="./../../designthinking/deliveryprocesses/resources/Ideacao_BDE44E63_e6b75c2a_ActivityDetail.jpeg"
+contentPage.processPage.elementUrls["_fnal0aK0EeubIrsk_5dxGg"] = ["Produto final","designthinking/workproducts/produto_978DECC6.html"];
+contentPage.processPage.elementUrls["_jnJy0aKpEeubIrsk_5dxGg"] = ["Designer","designthinking/roles/designer_EF2D955C.html"];
+contentPage.processPage.elementUrls["_CF-FsaKqEeubIrsk_5dxGg"] = ["Documento do problema","designthinking/workproducts/documento_do_problema_C7CF60B0.html"];
+contentPage.processPage.elementUrls["_7o2TcKK4EeubIrsk_5dxGg"] = ["equipe de design","designthinking/roles/equipe_design_3F8FE4DB.html"];
+contentPage.processPage.elementUrls["_VBbNpKKrEeubIrsk_5dxGg"] = ["Mural de possibilidades","designthinking/workproducts/mural_de_possibilidades_EBCA5895.html"];
+contentPage.processPage.elementUrls["_HKXO0aK5EeubIrsk_5dxGg"] = ["Cliente","designthinking/roles/cliente_DEB65BFE.html"];
+contentPage.processPage.elementUrls["_fO-f0aI0EeuEsYxgxLaCHg"] = ["DesignThinking","designthinking/deliveryprocesses/DesignThinking_FA13E873.html"];
+contentPage.processPage.elementUrls["_7o2Tc6K4EeubIrsk_5dxGg"] = ["Produto final","designthinking/workproducts/produto_978DECC6.html"];
+contentPage.processPage.elementUrls["_IQ8jwaK2EeubIrsk_5dxGg"] = ["equipe de design","designthinking/roles/equipe_design_3F8FE4DB.html"];
+contentPage.processPage.elementUrls["_F2aVEKI0EeuEsYxgxLaCHg"] = ["Compreender o problema","designthinking/tasks/compreender_o_problema_98296953.html"];
+contentPage.processPage.elementUrls["_DEH20KI4EeuEsYxgxLaCHg"] = ["Levantar requisitos","designthinking/tasks/levantar_requisitos_7D5A6480.html"];
+contentPage.processPage.elementUrls["_v3B0AaIwEeuEsYxgxLaCHg"] = ["Desenvolvimento","designthinking/capabilitypatterns/Desenvolvimento_99952B72.html"];
+contentPage.processPage.elementUrls["_ACwsQKK0EeubIrsk_5dxGg"] = ["ideia","designthinking/workproducts/ideia_A027C296.html"];
+contentPage.processPage.elementUrls["_pabnMaI0EeuEsYxgxLaCHg"] = ["Imersao","designthinking/deliveryprocesses/Imersao_C46982F1.html"];
+contentPage.processPage.elementUrls["_yFijdKKzEeubIrsk_5dxGg"] = ["Mural de possibilidades","designthinking/workproducts/mural_de_possibilidades_EBCA5895.html"];
+contentPage.processPage.elementUrls["_rUClcKI6EeuEsYxgxLaCHg"] = ["Prototipar a solucao","designthinking/tasks/prototipar_a_solucao_1621FCBA.html"];
+contentPage.processPage.elementUrls["_7o1sYaK4EeubIrsk_5dxGg"] = ["Designer","designthinking/roles/designer_EF2D955C.html"];
+contentPage.processPage.elementUrls["_il7TcaK2EeubIrsk_5dxGg"] = ["Prototipo","designthinking/workproducts/prototipo_B524F0E.html"];
+contentPage.processPage.elementUrls["_iiGSdKKtEeubIrsk_5dxGg"] = ["equipe de design","designthinking/roles/equipe_design_3F8FE4DB.html"];
+contentPage.processPage.elementUrls["_n4mvA6KrEeubIrsk_5dxGg"] = ["ideia","designthinking/workproducts/ideia_A027C296.html"];
+contentPage.processPage.elementUrls["_osLPEKKtEeubIrsk_5dxGg"] = ["Implantacao","designthinking/deliveryprocesses/Implantacao_91DB6EEE.html"];
+contentPage.processPage.elementUrls["_oUDN8KI0EeuEsYxgxLaCHg"] = ["Ideacao","designthinking/deliveryprocesses/Ideacao_BDE44E63.html"];
+contentPage.processPage.elementUrls["_zqSmEaI3EeuEsYxgxLaCHg"] = ["Compreensão do problema","designthinking/deliveryprocesses/Compreensão_580F61B0.html"];
+contentPage.processPage.elementUrls["_xhTKwaKvEeubIrsk_5dxGg"] = ["Testar","designthinking/deliveryprocesses/Testar_1854ED3.html"];
+contentPage.processPage.elementUrls["_obR0IaIwEeuEsYxgxLaCHg"] = ["Prototipacao","designthinking/capabilitypatterns/Prototipacao_DAAE9AB0.html"];
+contentPage.processPage.elementUrls["_Zm3_saK0EeubIrsk_5dxGg"] = ["Testar a solucao","designthinking/deliveryprocesses/Testar a solucao_E02A649.html"];
+contentPage.processPage.elementUrls["_gnmccaIwEeuEsYxgxLaCHg"] = ["Imersao","designthinking/capabilitypatterns/Imersao_9B805462.html"];
+contentPage.processPage.elementUrls["_iiGScaKtEeubIrsk_5dxGg"] = ["Designer","designthinking/roles/designer_EF2D955C.html"];
+contentPage.processPage.elementUrls["_fnZ-waK0EeubIrsk_5dxGg"] = ["equipe de design","designthinking/roles/equipe_design_3F8FE4DB.html"];
+contentPage.processPage.elementUrls["_KQxckaKtEeubIrsk_5dxGg"] = ["equipe de design","designthinking/roles/equipe_design_3F8FE4DB.html"];
+contentPage.processPage.elementUrls["_ACwsQ6K0EeubIrsk_5dxGg"] = ["Prototipo","designthinking/workproducts/prototipo_B524F0E.html"];
+contentPage.processPage.elementUrls["_VBbNoaKrEeubIrsk_5dxGg"] = ["Documento do problema","designthinking/workproducts/documento_do_problema_C7CF60B0.html"];
+contentPage.processPage.elementUrls["_cuEFcKI4EeuEsYxgxLaCHg"] = ["Selecionar ideias","designthinking/tasks/selecionar_ideias_3894CD6F.html"];
+contentPage.processPage.elementUrls["_tav2caI-EeuEsYxgxLaCHg"] = ["Apresentacao","designthinking/deliveryprocesses/Apresentacao_9A2D7384.html"];
+contentPage.processPage.elementUrls["_3COLMKI3EeuEsYxgxLaCHg"] = ["Compreender o problema","designthinking/tasks/compreender_o_problema_98296953.html"];
+contentPage.processPage.elementUrls["_N8zo4KI-EeuEsYxgxLaCHg"] = ["Definir o problema","designthinking/tasks/definir_o_problema_AA0CB0BF.html"];
+contentPage.processPage.elementUrls["_iiGSd6KtEeubIrsk_5dxGg"] = ["Produto final","designthinking/workproducts/produto_978DECC6.html"];
+contentPage.processPage.elementUrls["_gqYFYaK5EeubIrsk_5dxGg"] = ["Equipe de marketing","designthinking/roles/equipe_de_marketing_2A49C379.html"];
+contentPage.processPage.elementUrls["_4qcxAaKqEeubIrsk_5dxGg"] = ["equipe de design","designthinking/roles/equipe_design_3F8FE4DB.html"];
+contentPage.processPage.elementUrls["_k8EeIKI3EeuEsYxgxLaCHg"] = ["Coleta de Dados","designthinking/tasks/coleta_de_dados_B2041E25.html"];
+contentPage.processPage.elementUrls["_F2aVEKI0EeuEsYxgxLaCHg_xKHMEKKqEeubIrsk_5dxGg"] = ["equipe de design","designthinking/roles/equipe_design_3F8FE4DB.html"];
+contentPage.processPage.elementUrls["_j93eIaKzEeubIrsk_5dxGg"] = ["equipe de design","designthinking/roles/equipe_design_3F8FE4DB.html"];
+contentPage.processPage.elementUrls["_BMlGYaI4EeuEsYxgxLaCHg"] = ["Identificar requisitos","designthinking/deliveryprocesses/Identificar requisitos_F2108A12.html"];
+contentPage.processPage.elementUrls["_vdz2wKK0EeubIrsk_5dxGg"] = ["Implantar o produto no mercado/ambiente que foi observado o problema.","designthinking/tasks/implantar_o_produto_11F54466.html"];
+contentPage.processPage.elementUrls["_r1ynMaK0EeubIrsk_5dxGg"] = ["Implantar o produto","designthinking/deliveryprocesses/Implantar o produto_EC4762A4.html"];
+contentPage.processPage.elementUrls["_wA9nIaK0EeubIrsk_5dxGg"] = ["Divulgacao do produto","designthinking/deliveryprocesses/Divulgacao do produto_5BB63F93.html"];
+contentPage.processPage.elementUrls["_bFAxoKK0EeubIrsk_5dxGg"] = ["Testar a solucao","designthinking/tasks/testar_a_solucao_A8B08499.html"];
+contentPage.processPage.elementUrls["_JFYKAKK5EeubIrsk_5dxGg"] = ["Cliente","designthinking/roles/cliente_DEB65BFE.html"];
+contentPage.processPage.elementUrls["_qSuhIaI0EeuEsYxgxLaCHg"] = ["Prototipacao","designthinking/deliveryprocesses/Prototipacao_E74EE68D.html"];
+contentPage.processPage.elementUrls["_kNJm0aIwEeuEsYxgxLaCHg"] = ["Ideacao","designthinking/capabilitypatterns/Ideacao_56F0051E.html"];
+contentPage.processPage.elementUrls["_S4CJkKI6EeuEsYxgxLaCHg"] = ["Brainstorming","designthinking/tasks/brainstorming_F080017C.html"];
+contentPage.processPage.elementUrls["_yg-JYKI-EeuEsYxgxLaCHg"] = ["Apresentar as ideias","designthinking/tasks/apresentar_as_ideias_1BABC8.html"];
+contentPage.processPage.elementUrls["_KQxclKKtEeubIrsk_5dxGg"] = ["Designer","designthinking/roles/designer_EF2D955C.html"];
+contentPage.processPage.elementUrls["_FkHj0aI3EeuEsYxgxLaCHg"] = ["Empatizar ","designthinking/deliveryprocesses/Empatizar_3C64D07B.html"];
+contentPage.processPage.elementUrls["_qEFaAaKpEeubIrsk_5dxGg"] = ["Designer","designthinking/roles/designer_EF2D955C.html"];
+contentPage.processPage.elementUrls["_8boqcaI4EeuEsYxgxLaCHg"] = ["Construir solucao","designthinking/deliveryprocesses/Construir solucao_DBEFC56E.html"];
+contentPage.processPage.elementUrls["_mnRjEaI0EeuEsYxgxLaCHg"] = ["Desenvolvimento","designthinking/deliveryprocesses/Desenvolvimento_FF46CCB5.html"];
+contentPage.processPage.elementUrls["_CF9eoaKqEeubIrsk_5dxGg"] = ["Designer","designthinking/roles/designer_EF2D955C.html"];
+contentPage.processPage.elementUrls["_n4mvAKKrEeubIrsk_5dxGg"] = ["Mural de possibilidades","designthinking/workproducts/mural_de_possibilidades_EBCA5895.html"];
+contentPage.processPage.elementUrls["_n4mH8aKrEeubIrsk_5dxGg"] = ["equipe de design","designthinking/roles/equipe_design_3F8FE4DB.html"];
+contentPage.processPage.elementUrls["_C9VK8KI7EeuEsYxgxLaCHg"] = ["Desenvolver a solucao","designthinking/tasks/desenvolver_a_solucao_58BB89D.html"];
+contentPage.processPage.elementUrls["_bSyaQaI4EeuEsYxgxLaCHg"] = ["Decidir a ideia","designthinking/deliveryprocesses/Selecionar a ideia_90545E6D.html"];
+contentPage.processPage.elementUrls["_pcvFkaI6EeuEsYxgxLaCHg"] = ["Prototipar a solucao","designthinking/deliveryprocesses/Prototipar a solucao_F7DDE45F.html"];
+contentPage.processPage.elementUrls["_y8EGUKK0EeubIrsk_5dxGg"] = ["Divulgar o produto","designthinking/tasks/divulgar_o_produto_290BEBF1.html"];
+contentPage.processPage.elementUrls["_yFijcaKzEeubIrsk_5dxGg"] = ["equipe de design","designthinking/roles/equipe_design_3F8FE4DB.html"];
